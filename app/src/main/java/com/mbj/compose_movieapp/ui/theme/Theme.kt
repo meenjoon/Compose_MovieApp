b200922d9e11116cceb64f761680a5af113c587d
meenjoon <zqudwnsz@naver.com>
2023-09-12 19:16:02 +0900

    feat: Theme - Typography 설정

--- app/src/main/java/com/mbj/compose_movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/mbj/compose_movieapp/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.mbj.compose_movieapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
@@ -11,7 +13,9 @@ private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors }
 
 @Composable
 fun Compose_MovieAppTheme(
-    myColors: ColorSet,
+    myColors: ColorSet = ColorSet.Red,
+    typography: Typography = Typography,
+    shapes: Shapes = Shapes,
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
@@ -25,11 +29,11 @@ fun Compose_MovieAppTheme(
      * CompositionLocalProvider를 사용한 이유는 로컬 데이터를 공유하고 컴포저블 함수 간에 데이터를 전달하는 데 사용되며,
      * 특히 테마와 같이 앱의 모양과 느낌을 결정하는데 중요한 요소임
      */
-    CompositionLocalProvider(LocalColors provides colors ) {
+    CompositionLocalProvider(LocalColors provides colors) {
         MaterialTheme(
             colors = colors.material,
-            typography = Typography,
-            shapes = Shapes,
+            typography = typography,
+            shapes = shapes,
             content = content
         )
     }
