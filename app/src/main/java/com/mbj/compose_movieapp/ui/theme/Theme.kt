827e358620e62f535518f571e939e8737195b2bf
meenjoon <zqudwnsz@naver.com>
2023-09-12 18:54:02 +0900

    feat: Theme - Color 설정

--- app/src/main/java/com/mbj/compose_movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/mbj/compose_movieapp/ui/theme/Theme.kt
@@ -2,43 +2,35 @@ package com.mbj.compose_movieapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.mbj.compose_movieapp.ui.theme.color.ColorSet
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors }
 
 @Composable
-fun Compose_MovieAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun Compose_MovieAppTheme(
+    myColors: ColorSet,
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        myColors.DarkColors
     } else {
-        LightColorPalette
+        myColors.LightColors
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    /**
+     * CompositionLocalProvider를 사용한 이유는 로컬 데이터를 공유하고 컴포저블 함수 간에 데이터를 전달하는 데 사용되며,
+     * 특히 테마와 같이 앱의 모양과 느낌을 결정하는데 중요한 요소임
+     */
+    CompositionLocalProvider(LocalColors provides colors ) {
+        MaterialTheme(
+            colors = colors.material,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
