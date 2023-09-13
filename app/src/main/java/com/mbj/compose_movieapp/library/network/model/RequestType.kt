a7b998d267971438db19191986ab5a808a755f2e
meenjoon <zqudwnsz@naver.com>
2023-09-13 18:02:40 +0900

    feat: 네트워크 설정 파일 구현

---
+++ app/src/main/java/com/mbj/compose_movieapp/library/network/model/RequestType.kt
@@ -0,0 +1,8 @@
+package com.mbj.compose_movieapp.library.network.model
+
+enum class RequestType {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
---
+++ app/src/main/java/com/mbj/compose_movieapp/library/network/retrofit/NetworkRequestFactory.kt
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.part4plus.movieapp.library.network.retrofit
+
+import com.mbj.compose_movieapp.library.network.model.ApiResult
+import com.mbj.compose_movieapp.library.network.model.NetworkRequestInfo
+import java.lang.reflect.Type
+
+interface NetworkRequestFactory {
+
+    suspend fun <T> create(
+        url: String,
+        requestInfo: NetworkRequestInfo = NetworkRequestInfo.Builder().build(),
+        type: Type
+    ): ApiResult<T>
+}
