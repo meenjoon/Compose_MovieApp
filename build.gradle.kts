0caefd842faddd8dd9b74bd29bf1cc371e55b7da
meenjoon <zqudwnsz@naver.com>
2023-09-13 16:59:58 +0900

    chore: sealed class 오류 해결을 위한 프로젝트 수준의 gradle 파일 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+        maven{
+            url = uri("https://storage.googleapis.com/r8-releases/raw")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools:r8:8.2.16-dev")
+    }
+}
+
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
